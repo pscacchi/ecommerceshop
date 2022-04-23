a59ebc1027b75b7030b9ce1c735f03f399b8bdc0
PScacchi <pabloantonio-scacchi@endava.com>
2022-04-23 17:40:43 -0300

    Badge in the toolbar was added
    .gitignore improve
    
    A lot of dimens was added.
    
    Fix resources

--- app/src/main/java/ar/scacchipa/e_commerce/app/AppModule.kt
+++ app/src/main/java/ar/scacchipa/e_commerce/app/AppModule.kt
@@ -2,14 +2,12 @@ package ar.scacchipa.e_commerce.app
 
 import ar.scacchipa.e_commerce.repository.IItemRepository
 import ar.scacchipa.e_commerce.repository.MockFruitItemRepository
-import ar.scacchipa.e_commerce.viewmodel.ItemCartViewModel
 import org.koin.androidx.fragment.dsl.fragment
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     single<IItemRepository> { MockFruitItemRepository() as IItemRepository }
-    viewModel<ItemCartViewModel> { ItemCartViewModel() as ItemCartViewModel}
+//    viewModel<ItemCartViewModel> { ItemCartViewModel() as ItemCartViewModel}
     fragment<GondolaFragment> { GondolaFragment( get() ) }
     fragment<DetailFragment> { DetailFragment() }
     fragment<CartFragment> { CartFragment() }
